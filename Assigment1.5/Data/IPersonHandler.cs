c32ce9f08f5b615c7a567d1a816ba5ae278382f6
Frederik <frederik.m.hansen@gmail.com>
2021-10-31 20:59:33 +0100

    fixed måske

---
+++ Assigment1.5/Data/IPersonHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Models;
+
+namespace Assigment1._5.Data
+{
+	public interface IPersonHandler
+	{
+		Task NewAdult(Adult newAdult);
+		Task<String> RemoveAdult(int id);
+		Task<Adult> GetAdult(int id);
+		Task UpdateAdult(Adult updatedAdult);
+		Task<IList<Adult>> LoadAdults();
+	}
+}
---
+++ Assigment1.5/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace Assigment1._5.Models
+{
+	public class User
+	{
+		public string Username { get; set; }
+		public string Password { get; set; }
+		public int SecurityLevel { get; set; }
+
+		public User(string username, string password, int securityLevel)
+		{
+			Username = username;
+			Password = password;
+			SecurityLevel = securityLevel;
+		}
+	}
+}
